c8461b43a17e20eb8633055b00f9bd4628922dca
AlinaOktyabrskaya <44409263+AlinaOktyabrskaya@users.noreply.github.com>
2018-11-03 23:18:31 +0300

    Update Main.java

--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -1,7 +1,30 @@
 package blockchain;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
+
     public static void main(String[] args) {
-          new Blockchain();
+        new Blockchain();
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream("temp.out");
+            oos = new ObjectOutputStream(fos);
+        } catch (IOException e) {
+            System.out.println("Exit");
+        }
+        Blockchain b = new Blockchain();
+
+        try {
+            oos.writeObject(b);
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {
+            System.out.println("Smth wrong");
+        }
+
     }
-}
+    }
