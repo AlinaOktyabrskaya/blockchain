4ad3358144256d5b4172697b132aa7efd3c4b297
AlinaOktyabrskaya <44409263+AlinaOktyabrskaya@users.noreply.github.com>
2018-11-05 05:13:27 +0300

    Update Main.java

--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -2,6 +2,12 @@ package blockchain;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+          int miners_count = 5;
+
+       for (int i = 1; i <=miners_count;  i ++){
+           Thread thread = new Blockchain(i);
+           thread.start();
+       }
+
     }
 }
